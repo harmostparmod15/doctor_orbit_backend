de77e9f5cf91cd21b84cadea696a8ef3870deb64
= <harmostparmod@gmail.com>
2024-02-21 21:29:03 +0530

    bookingService - setup MVC && sequelize setup

---
+++ BookingService/src/migrations/20240221155526-create-bookings.js
@@ -0,0 +1,44 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("bookings", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      patientName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      patientAge: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      patientMobile: {
+        type: Sequelize.INTEGER,
+      },
+      doctorId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      appointmentDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("bookings");
+  },
+};
